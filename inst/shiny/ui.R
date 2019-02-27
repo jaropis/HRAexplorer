d2fee145c303cf6a960d3decc1223326a472757e
Jaroslaw Piskorski <jaropis@zg.home.pl>
2019-02-27 19:47:34 +0000

    added shinydashboard box (now working)

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,21 +1,32 @@
-ui <-
-  fluidPage(
-    titlePanel("Measures of the Poincare Plot"),
+library(shinydashboard)
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
     fluidRow(
-      column(3,
-             wellPanel(
-               data_infoUI("data-info")
-             )
-      ),
-      column(9,
-             tabsetPanel(
-               tabPanel("Poincare plot",
-                        plotsUI("plots")
-               ),
-               tabPanel("Numerical results", tags$style(type="text/css", "#filesView { overflow-x: auto; max-width: 100%; }"),
-                       main_tableUI("main-table"))
-
-             )
-      )
-      )
+      box(main_tableUI("main-table"))
+    )
   )
+)
+# ui <-
+#   fluidPage(
+#     titlePanel("Measures of the Poincare Plot"),
+#     fluidRow(
+#       column(3,
+#              wellPanel(
+#                data_infoUI("data-info")
+#              )
+#       ),
+#       column(9,
+#              tabsetPanel(
+#                tabPanel("Poincare plot",
+#                         plotsUI("plots")
+#                ),
+#                tabPanel("Numerical results", tags$style(type="text/css", "#filesView { overflow-x: auto; max-width: 100%; }"),
+#                        main_tableUI("main-table"))
+#
+#              )
+#       )
+#       )
+#   )
