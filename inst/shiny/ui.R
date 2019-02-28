c67b5cf384bfdb89135f927ce9051422afddc10e
Jarosław Piskorski <jaropis@zg.home.pl>
2019-03-01 00:22:44 +0100

    deadwood removed from ui.R

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -9,24 +9,3 @@ ui <- dashboardPage(
     )
   )
 )
-# ui <-
-#   fluidPage(
-#     titlePanel("Measures of the Poincare Plot"),
-#     fluidRow(
-#       column(3,
-#              wellPanel(
-#                data_infoUI("data-info")
-#              )
-#       ),
-#       column(9,
-#              tabsetPanel(
-#                tabPanel("Poincare plot",
-#                         plotsUI("plots")
-#                ),
-#                tabPanel("Numerical results", tags$style(type="text/css", "#filesView { overflow-x: auto; max-width: 100%; }"),
-#                        main_tableUI("main-table"))
-#
-#              )
-#       )
-#       )
-#   )
