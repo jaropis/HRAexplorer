4695eda06b0cee6c2e70888e7c9e5b008d072ea0
Jarosław Piskorski <jaropis@zg.home.pl>
2019-03-01 23:00:35 +0100

    styled the button and the rest of the flipBox

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,10 +1,21 @@
 library(shinydashboard)
+library(shinydashboardPlus)
 ui <- dashboardPage(
   dashboardHeader(title = "HRAExplorer"),
   dashboardSidebar(),
   dashboardBody(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "general.css")
+    ),
     fluidRow(
-      box(main_tableUI("main-table"), title = "Numerical results", solidHeader = TRUE, width = 12)
+      flipBox( id = 1,
+               front_title = "Numerical results",
+               solidHeader = FALSE,
+               width = 12,
+               main_tableUI("main-table"),
+               header_img = NULL,
+               main_img = NULL,
+               back_content = {})
     )
   )
 )
