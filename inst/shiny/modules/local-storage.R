df4f79e3f61f0ccec620dbabd868fd661b9c30c9
Jaroslaw Piskorski <jaropis@zg.home.pl>
2020-01-28 06:44:56 +0100

    RR_configuration moved to session

--- inst/shiny/modules/local-storage.R
+++ inst/shiny/modules/local-storage.R
@@ -3,9 +3,9 @@
 #' @param local_storage list containing data state of the application - this is the session$userData object
 #' @param data_key key for storing the data
 #' @param data_value data to be stored at data_key
-#' @export
 store_in_storage <- function(local_storage, data_key, data_value) {
   local_storage$userData[[data_key]] <- data_value
+  data_value # returning the data comes in handy
 }
 
 # this script provides local store - it is part of the shiny app so it is placed in the modules folder
