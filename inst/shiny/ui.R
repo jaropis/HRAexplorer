7b531e4435c0dfa84010914c731d8303c9f72d96
Jarosław Piskorski <jaropis@zg.home.pl>
2020-05-23 18:34:07 +0200

    no more modal, docker image run automatically

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,33 +1,46 @@
 ui <- dashboardPage(
   dashboardHeader(title = "HRAExplorer"),
   dashboardSidebar(
-      data_upload_and_filterUI("get-filter-data")
+      sidebarMenu(
+        id = "sidebar-tabs",
+        menuItem("Import and filter data",
+                 tabName = "data_import",
+                 icon = icon("table")),
+        menuItem("HRV time domain",
+                 tabName = "hrv_time_domain",
+                 icon = icon("heart"))
+      )
     ),
   dashboardBody(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "css/custom.css"),
       tags$script(src = "javascript/all.js")
     ),
-    fluidRow(
-      flipBox( id = 1,
-               front_title = "Numerical results",
-               solidHeader = FALSE,
-               width = 12,
-               main_tableUI("main-table"),
-               header_img = NULL,
-               main_img = NULL,
-               back_content = {
-                 fluidRow(
-                   box(title = "Poincare plot",
-                    #height = "500px",
-                    plotsUI("plots")
-                   ),
-                   box(title = "Poincare plot descriptors",
-                    #height = "500px",
-                    single_resultsUI("single-results")
-                   )
-                 )
-               })
+    tabItems(
+      tabItem(tabName = "data_import",
+              data_upload_and_filterUI("get-filter-data")
+              ),
+      tabItem (tabName = "hrv_time_domain",
+               fluidRow(
+                 flipBox( id = 1,
+                          front_title = "Numerical results",
+                          solidHeader = FALSE,
+                          width = 12,
+                          main_tableUI("main-table"),
+                          header_img = NULL,
+                          main_img = NULL,
+                          back_content = {
+                            fluidRow(
+                              box(title = "Poincare plot",
+                                  plotsUI("plots")
+                              ),
+                              box(title = "Poincare plot descriptors",
+                                  single_resultsUI("single-results")
+                              )
+                            )
+                          })
+               )
+      )
     )
   )
 )
