a26dc5a4c5e042d35cd87eac7a4282a1f880b6e2
Jarosław Piskorski <jaropis@zg.home.pl>
2020-10-12 00:57:48 +0200

    dynamic quality works

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -189,6 +189,29 @@ ui <- dashboardPage(
                            )
                          })
               )
+      ),
+      tabItem(tabName = "quality-dynamic",
+              fluidRow(
+                 flipBox(id = 8,
+                         front_title = "Numerical results",
+                         solidHeader = FALSE,
+                         width = 12,
+                         main_tableUI("main-table-quality-dynamic"),
+                         header_img = NULL,
+                         main_img = NULL,
+                         back_content = {
+                           fluidRow(
+                             box(title = "Poincare plot",
+                                 #plotsUI("plots")
+                                 tags$p("tyl 1")
+                             ),
+                             box(title = "Poincare plot descriptors",
+                                 tags$p("tyl 2")
+                                 #single_resultsUI("single-results")
+                             )
+                           )
+                         })
+              )
       )
     )
   )
